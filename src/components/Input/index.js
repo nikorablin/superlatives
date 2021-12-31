526473711c518335956857528229670132a02cdc
Nik Korablin <nikorablin@gmail.com>
2021-12-31 14:41:30 -0500

    fix: Show error on invalid input

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cx from 'classnames';
 
-const Input = props => (
-  <input
-    type="text"
-    {...props}
-    minLength={3}
-    required
-    className={cx(
-      'p-4',
-      'block',
-      'rounded',
-      'w-full',
-      'mb-2',
-      'text-black',
-    )}
-  />
-);
+const Input = ({ onChange, ...props }) => {
+  const [error, setError] = useState(false);
+  const onInvalid = () => setError(true);
+  const clearErrorOnChange = (evt) => {
+    setError(false);
+    onChange(evt);
+  }
+
+  return (
+    <div className="relative">
+      {error && <span className="absolute text-rose-600 text-3xl text-bold right-3 top-3">!</span>}
+      <input
+        {...props}
+        type="text"
+        onChange={clearErrorOnChange}
+        minLength={3}
+        required
+        onInvalid={onInvalid}
+        className={cx(
+          {
+            'border-rose-600': error,
+          },
+          'border-2',
+          'p-4',
+          'block',
+          'rounded',
+          'w-full',
+          'mb-2',
+          'text-black',
+        )}
+      />
+    </div>
+  );
+}
+  
 
 export default Input;
