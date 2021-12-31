03b647a73db61b9e54fb3041d5274acdb1fb2e7d
Nik Korablin <nikorablin@gmail.com>
2021-12-31 14:27:05 -0500

    fix: Validation on input

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import cx from 'classnames';
 
 const Input = props => (
   <input
     type="text"
     {...props}
-    className="p-4 block rounded w-full mb-2 text-black"
+    minLength={3}
+    required
+    className={cx(
+      'p-4',
+      'block',
+      'rounded',
+      'w-full',
+      'mb-2',
+      'text-black',
+    )}
   />
 );
 
